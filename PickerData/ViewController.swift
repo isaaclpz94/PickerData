d40b7f9123b6ac9edef302e1152152009e6c0435
isaaclpz94 <isaaclopez2894@hotmail.com>
2016-10-18 19:22:17 +0200

    PickerData

--- PickerData/ViewController.swift
+++ PickerData/ViewController.swift
@@ -8,18 +8,49 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController,UIPickerViewDataSource,UIPickerViewDelegate {
+    
+    let pickerData = [["10\"","14\"","18\"","24\""],["Cheese","Pepperoni","Sausage","Veggie","BBQ Chicken"]]
+    
+    enum PickerComponent:Int{
+        case size = 0
+        case topping = 1
+    }
+    
+    @IBOutlet weak var myLabel: UILabel!
+    @IBOutlet weak var myPicker: UIPickerView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        myPicker.delegate = self
+        myPicker.dataSource = self
+        myPicker.selectRow(2, inComponent: PickerComponent.size.rawValue, animated: false)
+        updateLabel()
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    //MARK -Delgates and DataSource
+    func numberOfComponentsInPickerView(pickerView: UIPickerView) -> Int {
+        return pickerData.count
     }
-
-
+    
+    func pickerView(pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return pickerData[component].count
+    }
+    
+    func pickerView(pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return pickerData[component][row]
+    }
+    
+    func pickerView(pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        updateLabel()
+    }
+    
+    func updateLabel(){
+        var sizeComponent = PickerComponent.size.rawValue
+        let toppingComponent = PickerComponent.topping.rawValue
+        let size = pickerData[sizeComponent][myPicker.selectedRowInComponent(sizeComponent)]
+        let topping = pickerData[toppingComponent][myPicker.selectedRowInComponent(toppingComponent)]
+        myLabel.text = "Pizza: " + size + " " + topping
+    }
 }
 
